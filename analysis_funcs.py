d44cbb89c6d95cfd56cf8a66623c9d7e05c7a2d6
Tianyue Zhao <zao@Tianyues-MacBook-Pro.local>
2020-02-08 15:54:56 -0800

    Edits to sorting method

--- analysis_funcs.py
+++ analysis_funcs.py
@@ -1,1 +1,12 @@
-import numpy as np
+def class_sort(Data, tot_classes, map):
+    sorted_data = []
+    target_column = 5
+    for i in range(tot_classes):
+        tmp = []
+        sorted_data.append(tmp)
+    for i in range(Data.shape[0]-1):
+        if(Data[i+1,target_column]==''):
+            print('Empty value, index '+i)
+            continue
+        sorted_data[map[i]].append(Data[i+1,target_column])
+    return sorted_data
--- basic_analysis.py
+++ basic_analysis.py
@@ -1,5 +1,7 @@
 from dataParser import *
 from label_funcs import *
+from analysis_funcs import *
 
 parsed_data = csvReader('barts_hotspots.csv')
 tot_classes,map = label_dow(parsed_data)
+sorted_data = class_sort(parsed_data, tot_classes, map)
