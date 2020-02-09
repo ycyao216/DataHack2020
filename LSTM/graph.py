10b002fef24b7aa5e9ef20a7a3c8d9d80f2910b3
Tianyue-Zhao <zhaotianyue@outlook.com>
2020-02-08 16:35:01 -0800

    Code now runs

--- LSTM/graph.py
+++ LSTM/graph.py
@@ -3,7 +3,8 @@ class Graph():
     def __init__(self):
         self.x_input = tf.placeholder("float", [None, 10, 3])
         self.y_gt = tf.placeholder("float", [None])
-        LSTM_layer = tf.keras.layers.CuDNNLSTM(3)
+        #LSTM_layer = tf.keras.layers.CuDNNLSTM(3)
+        LSTM_layer = tf.keras.layers.LSTM(3)
         pre_fc = LSTM_layer(self.x_input)
         W = tf.get_variable("FC_weights", shape=[3,1], dtype=tf.dtypes.float32, trainable=True)
         self.y_pred = tf.matmul(pre_fc, W)
--- LSTM/train.py
+++ LSTM/train.py
@@ -4,8 +4,8 @@ from data_loader import *
 
 path = 'Embarcadero, San Francisco, CA->Fisherman\'s Wharf, San Francisco, CA'
 save_directory = './models/'
-steps = 10
-save_interval = 5
+steps = 100
+save_interval = 10
 
 global_step = tf.Variable(0, name="step_count")
 graph = Graph()
@@ -14,10 +14,15 @@ loader = data_loader('../data/barts_hotspots_sorted.csv', path)
 optimizer = tf.train.AdamOptimizer().minimize(graph.mse_loss, global_step=global_step)
 saver = tf.train.Saver()
 with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
     for i in range(steps):
         x_input, y_input = loader.next_batch()
         sess.run(optimizer, feed_dict={graph.x_input:x_input,
             graph.y_gt:y_input})
-        if(i%save_interval):
+        if(i%save_interval==0):
             saver.save(sess, save_directory,
                     global_step=global_step)
+            x_input, y_input = loader.next_batch()
+            loss = sess.run(graph.mse_loss, feed_dict={graph.x_input:x_input,
+                graph.y_gt:y_input})
+            print(loss)
