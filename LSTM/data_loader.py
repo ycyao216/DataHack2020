02f1c9e7c8095957c91fb461e2ea22aa2a2afae9
Tianyue-Zhao <zhaotianyue@outlook.com>
2020-02-08 22:39:13 -0800

    More work on data loader

--- LSTM/data_loader.py
+++ LSTM/data_loader.py
@@ -2,15 +2,21 @@ import numpy as np
 from ../dataParser import *
 from random import randint
 class data_loader():
-    def __init__(file_name, path, jump=3, cutoff_percent=80, batch_size=20):
+    def __init__(file_name, path, jump=3, cutoff_percent=80, batch_size=20, days=10):
         self.jump = jump
         self.cutoff_percent = cutoff_precent
         self.batch_size = batch_size
         self.data_network = transNetwork(file_name)
         self.data_network.groupByConnection()
-        self.max_index = (len(self.data_network.network))*cutoff_percent/100
+        self.path = path
+        self.max_index = (len(self.data_network.avgPaths[path]))*cutoff_percent/100
         self.positions = max_index/jump
+        self.days = days
+        self.rows = self.data_network.avgPaths[path]
 
     def next_batch():
-        start_index = randint(0,positions)*jump+1
-        x = np.zeros((batch_size, 10, 3))
+        x = np.zeros((batch_size, self.days, 3))
+        for i in range(batch_size):
+            start_index = randint(0,positions)*jump+1
+            for k in range(self.days):
+                x[i,k,0] = self.rows[start_index+k][3]
