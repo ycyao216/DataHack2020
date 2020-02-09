1ca82d02048568024b8dee8ece7e65e9436c66b5
Tianyue-Zhao <zhaotianyue@outlook.com>
2020-02-08 17:14:03 -0800

    Added numpy output to csv

--- analysis_funcs.py
+++ analysis_funcs.py
@@ -1,3 +1,4 @@
+import math
 def class_sort(Data, tot_classes, map):
     sorted_data = []
     target_column = 5
@@ -5,8 +6,8 @@ def class_sort(Data, tot_classes, map):
         tmp = []
         sorted_data.append(tmp)
     for i in range(Data.shape[0]-1):
-        if(Data[i+1,target_column]==''):
-            print('Empty value, index '+i)
+        if(math.isnan(Data[i+1,target_column])):
+            print('Empty value, index '+str(i))
             continue
         sorted_data[map[i]].append(Data[i+1,target_column])
     return sorted_data
