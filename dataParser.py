918d4c195177e8be9e3a0e3e0376465ada5e99c1
Yunchao Yao <y8yao@ucsd.edu>
2020-02-08 18:25:48 -0800

    initialize structure of connections

--- dataParser.py
+++ dataParser.py
@@ -6,8 +6,37 @@ def csvReader(fileName):
     simpleArray = df.to_numpy()
     return simpleArray, df
 
-class BART:
-    None
-
 class connection:
-    None
+    def __init__(self,origin, destination, time = 0):
+        self.path = origin + "->" + destination
+        self.time = time
+
+class transNetwork:
+    def __init__(self, csv):
+        # list of connections
+        self.network = self.csvReader(csv)
+        #dict of unique connection nagems
+        self.avgPaths = self.getAllPath(self)
+        
+    def csvReader(self, csv):
+        df = pandas.DataForm();
+
+    def connectSame(self, connect1, connect2):
+        if (connect1.path == connect2.path):
+            return True
+        return False
+
+    def getAllPath(self):
+        paths = dict()
+        for connects in self.network:
+            paths[connects.path] = 0
+        return paths
+
+    def calculateAverage(self):
+        for cases in self.avgPaths:
+            sum = count = 0
+            for connections in self.network:
+                if(self.connectSame(self.avgPaths[cases],connections)):
+                    sum += connections.time
+                    count += 1
+            self.avgPaths[cases] = sum/count
